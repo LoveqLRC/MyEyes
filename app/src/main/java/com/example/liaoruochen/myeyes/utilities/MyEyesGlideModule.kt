42e86b6bce90a139332b454efd8719abcb0d9135
LoveqLRC <664215432@qq.com>
2018-11-13 09:33:11 +0800

    add note

--- app/src/main/java/com/example/liaoruochen/myeyes/utilities/MyEyesGlideModule.kt
+++ app/src/main/java/com/example/liaoruochen/myeyes/utilities/MyEyesGlideModule.kt
@@ -1,5 +1,6 @@
 package com.example.liaoruochen.myeyes.utilities
 
+import android.annotation.SuppressLint
 import android.app.ActivityManager
 import android.content.Context
 import android.content.Context.ACTIVITY_SERVICE
@@ -12,12 +13,14 @@ import com.bumptech.glide.load.DecodeFormat.PREFER_RGB_565
 
 @GlideModule
 class MyEyesGlideModule  :AppGlideModule(){
+    @SuppressLint("CheckResult")
     override fun applyOptions(context: Context, builder: GlideBuilder) {
 
         val defaultOptions =RequestOptions()
-        var activityManager = context.getSystemService(ACTIVITY_SERVICE) as ActivityManager
-
+        val activityManager = context.getSystemService(ACTIVITY_SERVICE) as ActivityManager
+        // Prefer higher quality images unless we're on a low RAM device
         defaultOptions.format(if (activityManager.isLowRamDevice) PREFER_RGB_565 else PREFER_ARGB_8888)
+        // Disable hardware bitmaps as they don't play nicely with Palette
         defaultOptions.disallowHardwareConfig()
 
         builder.setDefaultRequestOptions(defaultOptions)
