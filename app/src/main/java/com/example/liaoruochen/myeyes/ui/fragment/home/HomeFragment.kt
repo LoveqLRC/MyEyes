331bda9035f860047bb06f4bbd8cb0e60dda3960
LoveqLRC <664215432@qq.com>
2018-11-20 10:30:03 +0800

    update Home

--- app/src/main/java/com/example/liaoruochen/myeyes/ui/fragment/home/HomeFragment.kt
+++ app/src/main/java/com/example/liaoruochen/myeyes/ui/fragment/home/HomeFragment.kt
@@ -4,23 +4,41 @@ import com.example.liaoruochen.myeyes.R
 import com.example.liaoruochen.myeyes.ui.base.MultipleStatusFragment
 import kotlinx.android.synthetic.main.fragment_home.*
 
-class HomeFragment : MultipleStatusFragment() {
+class HomeFragment : MultipleStatusFragment() ,HomeContract.View{
+    override fun showLoading() {
+
+    }
+
+    override fun dimissLoading() {
+
+    }
+
+    private val mPresenter by lazy {
+        HomePresenter()
+    }
+
     override fun retry() {
 
     }
 
     override fun getLayoutId(): Int {
-       return R.layout.fragment_home
+        return R.layout.fragment_home
     }
 
     override fun initView() {
+        mPresenter.attachView(this)
         viewPager.pageMargin = 8
         viewPager.setPageMarginDrawable(R.drawable.page_margin)
     }
 
     override fun lazyLoad() {
+        mPresenter.requestCategories()
+
+    }
 
 
-
+    override fun onDestroy() {
+        super.onDestroy()
+        mPresenter.detachView()
     }
 }
--- app/src/main/java/com/example/liaoruochen/myeyes/ui/fragment/home/HomePresenter.kt
+++ app/src/main/java/com/example/liaoruochen/myeyes/ui/fragment/home/HomePresenter.kt
@@ -1,14 +1,27 @@
 package com.example.liaoruochen.myeyes.ui.fragment.home
 
+import android.util.Log
 import com.example.liaoruochen.myeyes.ui.base.BasePresenter
+import com.example.liaoruochen.myeyes.ui.fragment.home.model.HomeModel
+import io.reactivex.functions.Consumer
 
 class HomePresenter : BasePresenter<HomeContract.View>(), HomeContract.Presenter {
 
+    private val homeModel:HomeModel by lazy {
+        HomeModel()
+    }
 
     override fun requestCategories() {
         checkViewAttached()
         mRootView?.showLoading()
+        var disposable = homeModel.requestCategories()
+            .subscribe({ it ->
+
+            })
+        addSubscription(disposable)
     }
+
+
 
 
 }
